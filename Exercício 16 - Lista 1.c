4ae1a70e18c0dde8e2e0b6e06152840e2581541b
Yasmim-Mendes <143655110+Yasmim-Mendes@users.noreply.github.com>
2023-09-16 11:11:16 -0300

    Add files via upload

---
+++ Exercício 16 - Lista 1.c
@@ -0,0 +1,50 @@
+//Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e imprima o mês correspondente a este número. Isto é, janeiro se 1, fevereiro se 2, e assim por diante.
+
+#include <stdio.h>
+int main (){
+  int mes;
+
+  printf ("Informe um número de 1 a 12: ");
+  scanf ("%d", &mes);
+
+  switch (mes){
+    case 1: 
+      printf ("Janeiro");
+      break;
+    case 2: 
+      printf ("Fevereiro");
+      break;
+    case 3: 
+      printf ("Março");
+      break;
+    case 4: 
+      printf ("Abril");
+      break;
+    case 5:
+      printf ("Maio");
+      break;
+    case 6: 
+      printf ("Junho");
+      break;
+    case 7: 
+      printf ("Julho");
+      break;
+    case 8:
+      printf ("Agosto");
+      break;
+    case 9: 
+      printf ("Setembro");
+      break;
+    case 10: 
+      printf ("Outubro");
+      break;
+    case 11: 
+      printf ("Novembro");
+      break;
+    case 12: 
+      printf ("Dezembro");
+      break;
+  }
+
+  return 0;
+}
